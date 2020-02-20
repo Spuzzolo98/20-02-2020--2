e145395fe445d6459b4f8d963e51b22605f8a370
Lorenzo <60008393+Spuzzolo98@users.noreply.github.com>
2020-02-20 08:46:03 +0100

    scoperto il mondo
    /** è tipo utilissimo, wow

--- src/inserimentovettfunz/Inserimentovettfunz.java
+++ src/inserimentovettfunz/Inserimentovettfunz.java
@@ -4,25 +4,41 @@ import java.util.Scanner;
 
 public class Inserimentovettfunz {
 
-    static void inserimento(int vettoro[], int quanti) {
+    
+    /**
+     * inserimento degli oggetti nel vettore
+     * @param vector il vettore dal main in esame
+     * @param quanti numero di ogg del vettore
+     */
+    static void inserimento(int vector[], int quanti) { 
         Scanner kekkeroni = new Scanner(System.in);
         for (int i = 0; i < quanti; i++) {
-            vettoro[i] = kekkeroni.nextInt();
+            vector[i] = kekkeroni.nextInt();
         }
     }
-
-    static void output(int vettoro[], int quanti) {
+    
+    
+    /**
+     * output oggetti del vettore
+     * @param vettoro vettore dal main in esame
+     * @param quanti numero ogg vettore
+     */
+    static void output(int vettoro[], int quanti) { 
         for (int i = 0; i < quanti; i++) {
             System.out.println(vettoro[i]);
         }
     }
-
+    
+    
+    
     public static void main(String[] args) {
         Scanner kekkeroni = new Scanner(System.in);
-        int vector[] = new int[100], quanti = 0;
+        int quanti = 0; //int variabili/vettori
         quanti = kekkeroni.nextInt();
-        inserimento(vector, quanti);
+        int vector[] = new int[quanti];//determino quanti oggetti da usare
+        inserimento(vector, quanti); 
         output(vector, quanti);
     }
+    
 
 }
