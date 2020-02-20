d86bec624a90416c18c74f24055d11a28b894ffd
Lorenzo <60008393+Spuzzolo98@users.noreply.github.com>
2020-02-20 08:40:26 +0100

    formattato
    mancano ancora i commenti

--- src/inserimentovettfunz/Inserimentovettfunz.java
+++ src/inserimentovettfunz/Inserimentovettfunz.java
@@ -1,32 +1,28 @@
-
 package inserimentovettfunz;
+
 import java.util.Scanner;
 
-public class Inserimentovettfunz 
-{
+public class Inserimentovettfunz {
 
-    static void inserimento(int vettoro[],int quanti)
-    {
+    static void inserimento(int vettoro[], int quanti) {
         Scanner kekkeroni = new Scanner(System.in);
-        for(int i=0; i<quanti; i++)
-        {
+        for (int i = 0; i < quanti; i++) {
             vettoro[i] = kekkeroni.nextInt();
         }
     }
-    static void output(int vettoro[], int quanti)
-    {
-        for(int i=0; i<quanti; i++)
-        {
+
+    static void output(int vettoro[], int quanti) {
+        for (int i = 0; i < quanti; i++) {
             System.out.println(vettoro[i]);
         }
     }
-    public static void main(String[] args) 
-    {
+
+    public static void main(String[] args) {
         Scanner kekkeroni = new Scanner(System.in);
-        int vector[] = new int[100], quanti =0;
+        int vector[] = new int[100], quanti = 0;
         quanti = kekkeroni.nextInt();
-        inserimento(vector,quanti);
+        inserimento(vector, quanti);
         output(vector, quanti);
     }
-    
+
 }
